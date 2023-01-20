569080e80f1ddd6ec26136964fca444d4a7b96b6
KaziMdRabiulHossain <kazimdrabiulhossain9@gmail.com>
2023-01-20 12:26:47 +0600

    first commit

---
+++ flutter_application_4/lib/main.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(const Quizzler());
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText()!,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.green)),
+              onPressed: () {
+                bool? correctAnswers = quizBrain.getCorrectAnswer();
+
+                // ignore: unrelated_type_equality_checks
+                if (correctAnswers == true) {
+                  if (kDebugMode) {
+                    print('users got it right!');
+                  } else {
+                    if (kDebugMode) {
+                      print('users got it wrong');
+                    }
+                  }
+                }
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
+              },
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.red)),
+              onPressed: () {
+                bool? correctAnswers = quizBrain.getCorrectAnswer();
+
+                if (correctAnswers == false) {
+                  if (kDebugMode) {
+                    print('users got it right!');
+                  } else {
+
+                  }
+                  if (kDebugMode) {
+                    print('users got it wrong');
+                  }
+                }
+
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
+              },
+              child: const Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
+  }
+}
